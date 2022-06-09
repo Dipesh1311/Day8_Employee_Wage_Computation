6173b7306b7589891b9182c3d2bda9c8e664288b
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-06-09 18:05:55 +0530

    Added implemented program of UC3.

--- src/com/bridgelabz/empwagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/empwagecomputation/EmployeeWageComputation.java
@@ -1,22 +1,27 @@
 package com.bridgelabz.empwagecomputation;
+
 public class EmployeeWageComputation {
-        public static void main(String[] args) {
 
-            System.out.println("----- Welcome to Employee Wage Computation! -----");
+    public static void main(String[] args) {
 
-            int IS_PRESENT = 1;
-            int EMP_WAGE_PER_HOUR = 20;
+        System.out.println("----- Welcome to Employee Wage Computation! -----");
 
-            int empHrs, empWage;
-            double empCheck = Math.floor(Math.random() * 10) % 2;
-            if(empCheck == IS_PRESENT){
-                empHrs = 8;
-            }
-            else
-                empHrs = 0;
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+        int EMP_WAGE_PER_HOUR = 20;
 
-            empWage = empHrs * EMP_WAGE_PER_HOUR;
-            System.out.println("Emp Hours : "+empHrs);
-            System.out.println("Emp Wage : "+empWage);
-        }
+        int empHrs, empWage;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if(empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else if (empCheck == IS_PART_TIME)
+            empHrs = 4;
+        else
+            empHrs = 0;
+
+        empWage = empHrs * EMP_WAGE_PER_HOUR;
+        System.out.println("Emp Hours : "+empHrs);
+        System.out.println("Emp Wage : "+empWage);
+    }
+
 }
