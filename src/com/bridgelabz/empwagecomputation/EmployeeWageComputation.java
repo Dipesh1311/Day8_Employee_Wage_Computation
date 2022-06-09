5778904653e26a7bb5fe5753db6f3b4c57dd8186
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-06-09 18:09:23 +0530

    Added implemented program of UC4.

--- src/com/bridgelabz/empwagecomputation/EmployeeWageComputation.java
+++ src/com/bridgelabz/empwagecomputation/EmployeeWageComputation.java
@@ -2,22 +2,27 @@ package com.bridgelabz.empwagecomputation;
 
 public class EmployeeWageComputation {
 
+    private static final int IS_FULL_TIME = 1;
+    private static final int IS_PART_TIME = 2;
+    private static final int EMP_WAGE_PER_HOUR = 20;
+
     public static void main(String[] args) {
 
+        int empHrs, empWage;
+        int empType = (int)Math.floor(Math.random() * 10) % 3;
         System.out.println("----- Welcome to Employee Wage Computation! -----");
 
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_WAGE_PER_HOUR = 20;
-
-        int empHrs, empWage;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if(empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else if (empCheck == IS_PART_TIME)
-            empHrs = 4;
-        else
-            empHrs = 0;
+        switch(empType) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+                break;
+        }
 
         empWage = empHrs * EMP_WAGE_PER_HOUR;
         System.out.println("Emp Hours : "+empHrs);
